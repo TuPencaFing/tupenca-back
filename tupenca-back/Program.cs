fc1be0104b440a75d595854852647bcfa4b98d04
Valentina <valeshiro9@gmail.com>
2022-11-07 22:21:45 -0300

    url front user and front admin

--- tupenca-back/Program.cs
+++ tupenca-back/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       builder =>
                       {
-                          builder.WithOrigins("http://localhost:3000", "https://gentle-pond-0db98a110.2.azurestaticapps.net", "https://yellow-forest-0c32e6110.2.azurestaticapps.net").AllowAnyMethod().AllowAnyHeader();
+                          builder.WithOrigins("http://localhost:3000", " https://admin-tupenca-tsi.azurewebsites.net", "https://tupenca-user-front.azurewebsites.net").AllowAnyMethod().AllowAnyHeader();
                       });
 });
 // Add services to the container.
