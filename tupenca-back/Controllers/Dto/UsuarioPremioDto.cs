e975cd686515e313e6e48325e4b8f093ea80ae13
Valentina <valeshiro9@gmail.com>
2022-12-14 14:16:02 -0300

    fix usuario premio facturacion

--- tupenca-back/Controllers/Dto/UsuarioPremioDto.cs
+++ tupenca-back/Controllers/Dto/UsuarioPremioDto.cs
@@ -8,19 +8,19 @@ namespace tupenca_back.Controllers.Dto
 	{
         public int Id { get; set; }
 
-        public decimal Premio { get; set; }
+        public decimal? Premio { get; set; }
 
         public string? Banco { get; set; }
 
         public string? CuentaBancaria { get; set; }
 
-        public bool PendientePago { get; set; }
+        public bool? PendientePago { get; set; }
 
-        public bool Reclamado { get; set; }
+        public bool? Reclamado { get; set; }
 
-        public int IdUsuario { get; set; }
+        public int? IdUsuario { get; set; }
 
-        public int PencaId { get; set; }
+        public int? PencaId { get; set; }
 
         public Penca? Penca { get; set; }
 
